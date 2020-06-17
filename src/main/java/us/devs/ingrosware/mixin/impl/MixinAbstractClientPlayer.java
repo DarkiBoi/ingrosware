2be1f3cf13bc787bf6d3465c1415cbd7c3809c52
dashnullify <dashnullify@nullify.rip>
2020-06-17 15:56:27 -0700

    Made Profiles

---
+++ src/main/java/us/devs/ingrosware/mixin/impl/MixinAbstractClientPlayer.java
@@ -0,0 +1,34 @@
+package us.devs.ingrosware.mixin.impl;
+
+import net.minecraft.client.entity.AbstractClientPlayer;
+import net.minecraft.client.network.NetworkPlayerInfo;
+import net.minecraft.util.ResourceLocation;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.Shadow;
+import us.devs.ingrosware.IngrosWare;
+import us.devs.ingrosware.event.impl.other.EventCape;
+
+import javax.annotation.Nullable;
+
+/**
+ * made for Ingros
+ *
+ * @author Brennan
+ * @since 6/17/2020
+ **/
+@Mixin(AbstractClientPlayer.class)
+public abstract class MixinAbstractClientPlayer {
+    @Shadow
+    public abstract NetworkPlayerInfo getPlayerInfo();
+
+    @Overwrite
+    @Nullable
+    public ResourceLocation getLocationCape() {
+        final EventCape eventCape = new EventCape((AbstractClientPlayer) (Object) this);
+        IngrosWare.INSTANCE.getBus().post(eventCape);
+        NetworkPlayerInfo networkplayerinfo = this.getPlayerInfo();
+        return networkplayerinfo == null ? null : eventCape.isCancelled() ? eventCape.getResourceLocation() : networkplayerinfo.getLocationCape();
+    }
+
+}
