4e8b32d3c6771a0fd8cbf4d074ba2db9560ce21e
TBM <thegalaxygamesyt@gmail.com>
2020-06-22 10:49:24 +0100

    TimeModule

--- src/main/java/us/devs/ingrosware/mixin/impl/MixinWorld.java
+++ src/main/java/us/devs/ingrosware/mixin/impl/MixinWorld.java
@@ -7,15 +7,25 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import us.devs.ingrosware.IngrosWare;
 import us.devs.ingrosware.event.impl.other.GetRainStrengthEvent;
+import us.devs.ingrosware.event.impl.other.WorldTimeEvent;
 
 @Mixin(World.class)
 public class MixinWorld {
-
+    
     @Inject(method = "getRainStrength", at = @At("HEAD"), cancellable = true)
     public void getRainStrength(float delta, CallbackInfoReturnable<Float> cir) {
         GetRainStrengthEvent getRainStrengthEvent = new GetRainStrengthEvent();
         IngrosWare.INSTANCE.getBus().post(getRainStrengthEvent);
         if (getRainStrengthEvent.isCancelled()) cir.setReturnValue(0f);
+    }
+
+    @Inject(method = "getWorldTime", at = @At("HEAD"), cancellable = true)
+    public void getWorldTime(CallbackInfoReturnable<Long> cir) {
+        WorldTimeEvent worldTimeEvent = new WorldTimeEvent();
+        IngrosWare.INSTANCE.getBus().post(worldTimeEvent);
+        if (worldTimeEvent.isCancelled() && worldTimeEvent.getWorldTime() != null) {
+            cir.setReturnValue(worldTimeEvent.getWorldTime());
+        }
     }
 
 }
